0a9cd3f50d6d1982c3452f3777bdc376758670c2
Scott McNab <scott.mcnab33@gmail.com>
2024-08-14 15:25:15 -0600

    Data download, check for update, initialize all table correctly added

--- src/main/java/com/theornithologist/thecanticthrallnet/HelloApplication.java
+++ src/main/java/com/theornithologist/thecanticthrallnet/HelloApplication.java
@@ -2,6 +2,7 @@ package com.theornithologist.thecanticthrallnet;
 
 import com.theornithologist.thecanticthrallnet.datahandling.DataParser;
 import com.theornithologist.thecanticthrallnet.datahandling.DatabaseUpdater;
+import com.theornithologist.thecanticthrallnet.datahandling.FileConstants;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -21,12 +22,16 @@ public class HelloApplication extends Application {
 
         DataParser dataParser = new DataParser();
         DatabaseUpdater databaseUpdater = new DatabaseUpdater();
-//        databaseUpdater.generateDB();
-//        databaseUpdater.initializeTable();
-//////        databaseUpdater.populateFactions();
-//////        dataParser.parseFactionData();
+        databaseUpdater.generateDB();
+        databaseUpdater.initializeTable();
+//        databaseUpdater.populateFactions();
 //        databaseUpdater.populateUpdateTime();
-        System.out.println(databaseUpdater.updateRequired());
+//        databaseUpdater.fileUpdate();
+        System.out.println(databaseUpdater.generateTableString(FileConstants.FACTIONS_FILE));
+        System.out.println("CREATE TABLE IF NOT EXISTS Factions (" +
+                "   id TEXT," +
+                "   name TEXT" +
+                ");");
     }
 
     public static void main(String[] args) {
